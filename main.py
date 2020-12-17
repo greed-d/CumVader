28ca47945746dd8371c353bbd464a6a7cd833b1d
DeeXeeT-001 <deekshit.bti@gmail.com>
2020-12-17 18:19:30 +0545

    Score system and minor bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from random import randint
+import math
 
 # Initialize pygame
 pygame.init()
@@ -26,22 +27,24 @@ playerX_change = 0
 
 # Show enemy
 enemyImg = pygame.image.load("./ass.png")
-enemyImg = pygame.transform.scale(enemyImg, (128, 128))
-enemyX = randint(0, 800)
+enemyImg = pygame.transform.scale(enemyImg, (100, 100))
+enemyX = randint(0, 729)
 enemyY = randint(50, 150)
-enemyX_change = 1.5
+enemyX_change = 1
 enemyY_change = 15
 
 # Show Bullet
 # Ready = You can't see the bullet
 # Fire = The bullet is moving
 bulletImg = pygame.image.load("./cumBullet.png")
-bulletImg = pygame.transform.scale(bulletImg, (32, 32))
+bulletImg = pygame.transform.scale(bulletImg, (54, 54))
 bulletX = 0
 bulletY = 480
 bulletX_change = 0
-bulletY_change = 7
+bulletY_change = 5
 bullet_state = "ready"
+
+score = 0
 
 
 def player(x, y):
@@ -58,12 +61,23 @@ def bulletFire(x, y):
     screen.blit(bulletImg, (x + 16, y + 10))
 
 
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(
+        (math.pow(enemyX - bulletX, 2)) + (math.pow(enemyY - bulletY, 2))
+    )
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
 # Main loop
 def mainLoop():
     global screen
     global playerX, playerY, playerX_change
     global enemyX, enemyY, enemyX_change, enemyY_change
-    global bulletY, bulletY_change
+    global bulletY, bulletY_change, bullet_state, bulletX
+    global score
 
     running = True
     while running:
@@ -85,7 +99,9 @@ def mainLoop():
                     playerX_change = 5
 
                 if event.key == pygame.K_SPACE:
-                    bulletFire(playerX, playerY)
+                    if bullet_state is "ready":
+                        bulletX = playerX
+                        bulletFire(bulletX, playerY)
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -95,21 +111,35 @@ def mainLoop():
 
         if playerX <= 0:
             playerX = 0
-        elif playerX >= 736:
-            playerX = 736
+        elif playerX >= 729:
+            playerX = 729
 
         enemyX += enemyX_change
 
         if enemyX <= 0:
-            enemyX_change = 3
+            enemyX_change = 1
             enemyY += enemyY_change
-        elif enemyX >= 736:
-            enemyX_change = -3
+        elif enemyX >= 729:
+            enemyX_change = -1
             enemyY += enemyY_change
 
+        if bulletY <= 0:
+            bulletY = 480
+            bullet_state = "ready"
+
         if bullet_state is "fire":
-            bulletFire(playerX, bulletY)
+            bulletFire(bulletX, bulletY)
             bulletY -= bulletY_change
+
+        # Collision
+        collision = isCollision(enemyX, enemyY, bulletX, bulletY)
+        if collision:
+            bulletY = 480
+            bullet_state = "ready"
+            score += 1
+            print(score)
+            enemyX = randint(0, 729)
+            enemyY = randint(50, 150)
 
         player(playerX, playerY)
         enemy(enemyX, enemyY)
