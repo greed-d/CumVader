816cb3bcbbb72d8e686a3ca88d18de8d728ead91
DeeXeeT-001 <deekshit.bti@gmail.com>
2020-12-17 16:59:20 +0545

    Modifications to my need

--- main.py
+++ main.py
@@ -18,17 +18,30 @@ pygame.display.set_icon(icon)
 background = pygame.image.load("./background.jpg")
 
 # Show player
-playerImg = pygame.image.load("./spaceship.png")
+playerImg = pygame.image.load("./penis.png")
+playerImg = pygame.transform.scale(playerImg, (64, 64))
 playerX = 370
 playerY = 480
 playerX_change = 0
 
 # Show enemy
-enemyImg = pygame.image.load("./alien.png")
+enemyImg = pygame.image.load("./ass.png")
+enemyImg = pygame.transform.scale(enemyImg, (128, 128))
 enemyX = randint(0, 800)
 enemyY = randint(50, 150)
-enemyX_change = 3
-enemyY_change = 40
+enemyX_change = 1.5
+enemyY_change = 15
+
+# Show Bullet
+# Ready = You can't see the bullet
+# Fire = The bullet is moving
+bulletImg = pygame.image.load("./cumBullet.png")
+bulletImg = pygame.transform.scale(bulletImg, (32, 32))
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 7
+bullet_state = "ready"
 
 
 def player(x, y):
@@ -39,16 +52,18 @@ def enemy(x, y):
     screen.blit(enemyImg, (x, y))
 
 
+def bulletFire(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+
 # Main loop
 def mainLoop():
     global screen
-    global playerX
-    global playerY
-    global playerX_change
-    global enemyX
-    global enemyY
-    global enemyX_change
-    global enemyY_change
+    global playerX, playerY, playerX_change
+    global enemyX, enemyY, enemyX_change, enemyY_change
+    global bulletY, bulletY_change
 
     running = True
     while running:
@@ -68,6 +83,9 @@ def mainLoop():
 
                 if event.key == pygame.K_RIGHT:
                     playerX_change = 5
+
+                if event.key == pygame.K_SPACE:
+                    bulletFire(playerX, playerY)
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -89,10 +107,9 @@ def mainLoop():
             enemyX_change = -3
             enemyY += enemyY_change
 
-        # if enemyX <= 0:
-        #     enemyY_change = 0.3
-        # elif enemyX >= 736:
-        #     enemyY_change = 0.3
+        if bullet_state is "fire":
+            bulletFire(playerX, bulletY)
+            bulletY -= bulletY_change
 
         player(playerX, playerY)
         enemy(enemyX, enemyY)
